fa109114d2015cc2314660d1beb109050ae556d7
xusupeng <xusupeng@beisen.com>
2020-04-19 21:01:41 +0800

    feat(UserLoginController): 去除QQ登陆
    现在QQ登陆不是我们需要的登陆方式，徒增复杂度

--- easyIM-api/src/main/java/top/xxpblog/easyChat/api/controller/user/UserLoginController.java
+++ easyIM-api/src/main/java/top/xxpblog/easyChat/api/controller/user/UserLoginController.java
@@ -2,21 +2,15 @@ package top.xxpblog.easyChat.api.controller.user;
 
 
 import io.swagger.annotations.ApiOperation;
-import top.xxpblog.easyChat.api.service.other.QqWebAuthService;
-import top.xxpblog.easyChat.api.service.user.UserQqService;
 import top.xxpblog.easyChat.api.utils.UserLoginUtils;
 import top.xxpblog.easyChat.api.vo.req.UserLoginPwdReqVO;
-import top.xxpblog.easyChat.api.vo.res.QqOpenIdResVO;
-import top.xxpblog.easyChat.api.vo.res.QqUserInfoResVO;
 import top.xxpblog.easyChat.api.vo.res.UserLoginResVO;
 import top.xxpblog.easyChat.api.service.user.UserService;
 import top.xxpblog.easyChat.api.utils.PasswordUtils;
 import top.xxpblog.easyChat.common.entity.user.User;
-import top.xxpblog.easyChat.common.entity.user.UserQq;
 import top.xxpblog.easyChat.common.enums.ResultEnum;
 import top.xxpblog.easyChat.common.vo.res.BaseResVO;
 import top.xxpblog.easyChat.common.utils.ResultVOUtils;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,11 +27,6 @@ public class UserLoginController {
     @Resource
     private UserService userService;
 
-    @Resource
-    private QqWebAuthService qqWebAuthService;
-
-    @Resource
-    private UserQqService userQqService;
 
     @ApiOperation("账号密码登陆")
     @PostMapping("/byPwd")
@@ -81,60 +70,6 @@ public class UserLoginController {
         return ResultVOUtils.success(userLoginResVO);
     }
 
-    @ApiOperation("QQ登陆")
-    @PostMapping("/byQq")
-    public BaseResVO byQq(@RequestParam(value = "code") String code,
-                          @RequestParam(value = "redirect_uri") String redirect_uri) {
-
-
-        String accessToken = qqWebAuthService.getAccessToken(code, redirect_uri);
-        if (accessToken == null || "".equals(accessToken)) {
-            return ResultVOUtils.error(ResultEnum.PARAM_VERIFY_FALL, "accessToken 获取失败~");
-        }
-
-        QqOpenIdResVO openIdResVO = qqWebAuthService.getOpenID(accessToken);
-        if (openIdResVO == null || openIdResVO.getOpenid() == null || "".equals(openIdResVO.getOpenid())) {
-            return ResultVOUtils.error(ResultEnum.PARAM_VERIFY_FALL, "openid 获取是吧~");
-        }
-        QqUserInfoResVO userInfo = qqWebAuthService.getUserInfo(accessToken, openIdResVO.getOpenid());
-
-        if (userInfo == null) {
-            return ResultVOUtils.error(ResultEnum.PARAM_VERIFY_FALL, "userInfo 获取失败~");
-        }
-
-        UserQq userQq = userQqService.findByOpenId(openIdResVO.getOpenid());
-
-        // 判断是否有过值
-        if (userQq == null || userQq.getUid() == null) {
-            // 创建用户
-            User user = new User();
-            user.setName(userInfo.getNickname());
-            user.setAvatar(userInfo.getFigureurl());
-            String remark = "你今生有没有坚定不移地相信过一件事或一个人？是那种至死不渝的相信？";
-            user.setRemark(remark);
-            boolean b = userService.insertUser(user);
-            if (!b) {
-                return ResultVOUtils.error();
-            }
-            // 创建QQ用户
-            userQq = new UserQq();
-            userQq.setUid(user.getUid());
-            userQq.setOpenid(openIdResVO.getOpenid());
-            boolean b1 = userQqService.insertUserQq(userQq);
-            if (!b1) {
-                return ResultVOUtils.error();
-            }
-        }
-
-        Long uid = userQq.getUid();
-
-        String sid = UserLoginUtils.createSid(uid);
-
-        UserLoginResVO userLoginResVO = new UserLoginResVO();
-        userLoginResVO.setUid(uid);
-        userLoginResVO.setSid(sid);
-        return ResultVOUtils.success(userLoginResVO);
-    }
 
 
 }
